a9a22077296f3526d1eeece4b55cdb8d35f29498
Eric Cosme Damião Martins Souza <eric.martins2010@yahoo.com.br>
2023-05-02 11:40:18 -0300

    Initial commit

---
+++ src/fundamentos/PrimeiroProgramaJavaUdemy.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class PrimeiroProgramaJavaUdemy {
+    public static void main(String[] args) {
+
+        System.out.println("Primeiro programa do curso Java do Udemy!");
+    }
+}
