59b6b8ed777ec591b873d0ce1b619721cb283c62
Eric Cosme Damião Martins Souza <eric.martins2010@yahoo.com.br>
2023-05-04 19:25:24 -0300

    Adicionando nova saída

--- src/fundamentos/PrimeiroProgramaJavaUdemy.java
+++ src/fundamentos/PrimeiroProgramaJavaUdemy.java
@@ -3,6 +3,7 @@ package fundamentos;
 public class PrimeiroProgramaJavaUdemy {
     public static void main(String[] args) {
 
-        System.out.println("Primeiro programa do curso Java do Udemy!");
+        System.out.println("Primeiro programa do curso Java do Udemy Parte #01!");
+        System.out.println("Primeiro programa do curso Java do Udemy Parte #02!");
     }
 }
